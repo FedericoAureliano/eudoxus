abb4fd07db4caae18222cade73c4d6cd6869e8a1
adwait <adwait@berkeley.edu>
2024-04-22 20:44:43 -0700

    Add MaxRes implementation

--- src/eudoxus/checker/interface.py
+++ src/eudoxus/checker/interface.py
@@ -81,3 +81,63 @@ class Checker:
                     positions.append(core[0])
                 case _:
                     raise ValueError("Must be sat or unsat!")
+
+    def solve_maxres(self) -> Tuple[List[Position], z3.ModelRef]:
+        """
+        Check the constraints and return the list of positions that need to be
+        changed and a model for inference.
+        """
+
+        result = z3.unsat
+        positions = []
+
+        conj_soft_constraints = {
+            pos: z3.And(*constraints)
+            for pos, constraints in self.soft_constraints.items()
+        }
+
+        live_constraints = {pos2str(pos): c for pos, c in conj_soft_constraints.items()}
+
+        while result != z3.sat:
+            self.solver_reset()
+            for constraint in self.hard_constraints:
+                self.solver.add(constraint)
+            # After applying MaxRes
+            for name, constraint in live_constraints.items():
+                self.solver.assert_and_track(constraint, name)
+
+            result = self.solver.check()
+            match result:
+                case z3.sat:
+                    model = self.solver.model()
+                    positions = [
+                        pos
+                        for pos, cons in conj_soft_constraints.items()
+                        if not model.evaluate(cons)
+                    ]
+                    return positions, model
+                case z3.unsat:
+                    core = self.solver.unsat_core()
+                    if len(core) == 0:
+                        raise ValueError("If unsat, there must be a core!")
+                    core = sorted([str(c) for c in core])
+                    non_core_constraints = {
+                        name: cons
+                        for name, cons in live_constraints.items()
+                        if name not in core
+                    }
+                    if len(core) == 1:
+                        live_constraints = non_core_constraints
+                    else:
+                        maxres_constraints = []
+                        prefix = z3.BoolVal(core[0])
+                        for clause in core[1:]:
+                            new_clause = z3.Or(prefix, live_constraints[clause])
+                            prefix = z3.And(prefix, live_constraints[clause])
+                            maxres_constraints.append((str(new_clause), new_clause))
+                        live_constraints = non_core_constraints
+                        live_constraints.update(
+                            {name: cons for (name, cons) in maxres_constraints}
+                        )
+                case _:
+                    raise ValueError("Must be sat or unsat!")
