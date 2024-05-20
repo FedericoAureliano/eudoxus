7370edf52d5c0b50414bd6b0524ce3dc3a8839cb
FedericoAureliano <fmorarocha@gmail.com>
2024-05-19 17:37:14 -0700

    more aggresive duplicate checking

--- src/eudoxus/repair/duplicates.py
+++ src/eudoxus/repair/duplicates.py
@@ -15,21 +15,28 @@ class DuplicateChecker(Checker):
         rewrites = {}
 
         for module in modules:
-            for component in [
+            components = [
                 module.instances,
-                module.types,
                 module.inputs,
                 module.outputs,
                 module.sharedvars,
                 module.locals,
-            ]:
+                module.types,
+            ]
+            for i in range(len(components)):
+                component = components[i]
+                existing_names = set()
+                for j in range(i):
+                    other = components[j]
+                    for other_component in other.statements:
+                        existing_names.add(other_component.target.name)
+
                 component_pos = component.position
-                component_names = set()
-                new_components = []
+                new_decls = []
                 for component in component.statements:
-                    if component.target.name not in component_names:
-                        component_names.add(component.target.name)
-                        new_components.append(component)
-                rewrites[component_pos] = s.Block(component_pos, new_components)
+                    if component.target.name not in existing_names:
+                        existing_names.add(component.target.name)
+                        new_decls.append(component)
+                rewrites[component_pos] = s.Block(component_pos, new_decls)
 
         return [rewrites]
